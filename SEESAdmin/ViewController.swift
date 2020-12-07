5b70429c21b90c9169c69dc7989b697f7ff59f96
Robert Parsons <robertparsons4@icloud.com>
2020-12-06 16:50:15 -0800

    Initial Commit

---
+++ SEESAdmin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SEESAdmin
+//
+//  Created by Robert Parsons on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
