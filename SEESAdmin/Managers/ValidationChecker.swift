703c47b9850e83ae1b370d541ffe970bbddaf40b
Robert Parsons <robertparsons4@icloud.com>
2021-01-18 20:14:30 -0800

    Added validation for Contacts and adjusted the other validations to be more specific.

--- SEESAdmin/Managers/ValidationChecker.swift
+++ SEESAdmin/Managers/ValidationChecker.swift
@@ -13,7 +13,7 @@ struct ValidationChecker {
         case .students: return validateStudentData(with: dictionary)
         case .options: return validateOptionData(with: dictionary)
         case .events: return validateEventData(with: dictionary)
-        case .contacts: return nil
+        case .contacts: return validateContactData(with: dictionary)
         }
     }
     
@@ -27,7 +27,7 @@ struct ValidationChecker {
             switch key {
             case dataType.firstName.key, dataType.lastName.key:
                 if value.isEmpty {
-                    errorString.append("\n\nEnsure the student has a first and last name.")
+                    errorString.append("\n\nEnsure the student has a \(key).")
                 }
             case dataType.email.key:
                 if !(value ~= Validations.email.regex) {
@@ -54,7 +54,7 @@ struct ValidationChecker {
             switch key {
             case dataType.majorName.key, dataType.optionName.key:
                 if value.isEmpty {
-                    errorString.append("\n\nEnsure there is a major name and an option name.")
+                    errorString.append("\n\nEnsure \(key) has a value.")
                 }
             case dataType.curriculumSheet.key, dataType.flowchart.key, dataType.roadMap.key:
                 if !(value ~= Validations.link.regex) {
@@ -78,13 +78,9 @@ struct ValidationChecker {
             let key = data.key, value = dictionary[key] as! String
             
             switch key {
-            case dataType.eventName.key:
+            case dataType.eventName.key, dataType.date.key:
                 if value.isEmpty {
-                    errorString.append("\n\nEnsure there is an event name.")
-                }
-            case dataType.date.key:
-                if value.isEmpty {
-                    errorString.append("\n\nEnsure there is a date.")
+                    errorString.append("\n\nEnsure \(key) has a value.")
                 }
             case address, city, state, zip, country:
                 guard !incompleteAddress else { break }
@@ -105,17 +101,59 @@ struct ValidationChecker {
         
         return errorString.isEmpty ? nil : .unableToValidate(error: errorString)
     }
+    
+    private static func validateContactData(with dictionary: [String: Any]) -> SEESError? {
+        var errorString: String = ""
+        let dataType = FBContact.self
+        
+        for data in dataType.allCases {
+            let key = data.key, value = dictionary[key] as! String
+            
+            switch key {
+            case dataType.fullName.key, dataType.title.key, dataType.office.key:
+                if value.isEmpty {
+                    errorString.append("\n\nEnsure \(key) contains a value.")
+                }
+            case dataType.phone.key:
+                if !(value ~= Validations.phone.regex) {
+                    errorString.append(Validations.phone.error)
+                }
+            case dataType.email.key:
+                if !(value ~= Validations.email.regex) {
+                    errorString.append(Validations.email.error)
+                }
+            case dataType.order.key:
+                if !(value ~= Validations.order.regex) {
+                    errorString.append(Validations.order.error)
+                }
+            case dataType.monday.key, dataType.tuesday.key, dataType.wednesday.key, dataType.thursday.key, dataType.friday.key:
+                guard !value.isEmpty else { break }
+                let times = value.components(separatedBy: "-")
+                if let startTime = times.getItemAt(0)?.converToTime(), let endTime = times.getItemAt(1)?.converToTime() {
+                    if startTime > endTime {
+                        errorString.append("\n\nEnsure that \(key)'s start time is before it's end time.")
+                    }
+                }
+            default: break
+            }
+        }
+        
+        return errorString.isEmpty ? nil : .unableToValidate(error: errorString)
+    }
 }
 
 enum Validations {
     case email, broncoID
     case link
+    case phone, order
     
     var regex: String {
         switch self {
         case .email: return "^[a-zA-Z0-9]+@cpp.edu$"
         case .broncoID: return "^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$"
         case .link: return "^https://www.cpp.edu"
+        case .phone: return "^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$"
+        case .order: return "^[0-9]+$"
         }
     }
     
@@ -124,6 +162,8 @@ enum Validations {
         case .email: return "\n\nEnsure the email only contains letters and numbers and uses \"@cpp.edu\"."
         case .broncoID: return "\n\nEnsure the Bronco ID is only digits and is 9 digits long."
         case .link: return "\n\nEnsure all three of the links are CPP links with this format: \n\"https://www.cpp.edu/...\""
+        case .phone: return "\n\nEnsure the phone number is 10 digits long and doesn't contain any special characters."
+        case .order: return "\n\nEnsure the order is a unique, positive integer."
         }
     }
 }
